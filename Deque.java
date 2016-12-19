0101e12cef5bc631a7e9139d6fe7915ce2282890
unknown <amanagrawala@gmail.com>
2016-12-19 15:47:42 -0600

    Fix bug with previous linked list not being recorded.

--- Deque.java
+++ Deque.java
@@ -1,5 +1,8 @@
 import java.util.Iterator;
 import java.util.NoSuchElementException;
+import java.lang.NullPointerException;
+import java.lang.UnsupportedOperationException;
+
 
 public class Deque<Item> implements Iterable<Item> {
     
@@ -23,7 +26,7 @@ public class Deque<Item> implements Iterable<Item> {
     
     public boolean isEmpty()
     {
-        return (first == null && last == null);
+        return (size == 0);
     }
     
     public int size()
@@ -33,6 +36,10 @@ public class Deque<Item> implements Iterable<Item> {
     
     public void addFirst(Item item)
     {
+        if(item == null)
+        {
+            throw new NullPointerException();
+        }
         Node old = first;
         first = new Node();
         first.item = item;
@@ -43,10 +50,18 @@ public class Deque<Item> implements Iterable<Item> {
         {
             last = first;
         }
+        else
+        {
+            old.backward = first;
+        }
     }
     
     public void addLast(Item item)
     {
+        if(item == null)
+        {
+            throw new NullPointerException();
+        }
         Node old = last;
         last = new Node();
         last.item = item;
@@ -65,18 +80,44 @@ public class Deque<Item> implements Iterable<Item> {
     
     public Item removeFirst()
     {
+        if(size() == 0)
+        {
+            throw new NoSuchElementException();
+        }
         Item value = first.item;
         first = first.forward;
+        if(first != null)
+        {
+            first.backward = null;
+        }
         size--;
+        if(size() == 0)
+        {
+            last = null;
+        }
         return value;
     }
     
     public Item removeLast()
     {
+        if(size() == 0)
+        {
+            throw new NoSuchElementException();
+        }
         Item item = last.item;
         last = last.backward;
+        if(last != null)
+        {
+            last.forward = null;   
+        }
         size--;
+        if(size() == 0)
+        {
+            first = null;
+        }
         return item;
+        
+
     }
     
     public Iterator<Item> iterator()
@@ -95,11 +136,19 @@ public class Deque<Item> implements Iterable<Item> {
         
         public void remove()
         {
-            /* not supported */
+            throw new UnsupportedOperationException();
         }
         
         public Item next()
         {
+            try
+            {
+                Node attempt = current.forward;
+            }
+            catch(NullPointerException e)
+            {
+                throw new NoSuchElementException();
+            }
             Item item = current.item;
             current = current.forward;
             return item;
@@ -109,15 +158,40 @@ public class Deque<Item> implements Iterable<Item> {
     public static void main(String[] args)
     {
         Deque<String> test = new Deque<String>();
-        test.addLast("to");
-        test.addLast("be");
-        test.addLast("or");
-        test.addLast("not");
-        for(String s: test)
-        {
-            System.out.print(s);
-        }
+        test.addFirst("to");
+        test.addFirst("be");
+        test.removeLast();
+        test.removeLast();
+        test.removeLast();
+        
+//        test.addLast("not");
+//        for(String s: test)
+//        {
+//            System.out.print(s);
+//        }
+//        Iterator<String> i = test.iterator();
+//        while (i.hasNext())
+//        {
+//            String s = i.next();
+//            StdOut.println(s);
+//        }
+//        i.next();
         //String value = test.removeFirst();
         //System.out.print(value);
+        
+//        Deque<Integer> test10 = new Deque<Integer>();
+//        test10.addFirst(1);
+//        test10.addLast(2);
+//        test10.addFirst(3);
+//        test10.addFirst(4);
+//        test10.removeFirst();
+//        test10.addLast(6);
+//        test10.removeFirst();
+//        test10.addFirst(8);
+//        test10.removeLast();
+//        for(Integer s: test10)
+//        {
+//            System.out.print(s);
+//        }
     }
 }
--- RandomizedQueue.java
+++ RandomizedQueue.java
@@ -1,14 +1,18 @@
 import edu.princeton.cs.algs4.StdRandom;
 import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.lang.NullPointerException;
+import java.lang.UnsupportedOperationException;
 
 public class RandomizedQueue<Item> implements Iterable<Item>
 {
     private Item[] q;
     private int N = 0;
+   // private int startLoc = 9;
         
     public RandomizedQueue()
     {
-        q = (Item[]) new Object[10];
+        q = (Item[]) new Object[1];
     }
     
     public boolean isEmpty()
@@ -33,6 +37,10 @@ public class RandomizedQueue<Item> implements Iterable<Item>
     
     public void enqueue(Item item)
     {
+        if(item == null)
+        {
+            throw new NullPointerException();
+        }
         if( N == q.length)
         {
             resize(2*q.length);
@@ -42,11 +50,17 @@ public class RandomizedQueue<Item> implements Iterable<Item>
     
     public Item dequeue()
     {
+        if(size() == 0)
+        {
+            throw new NoSuchElementException();
+        }
         int rand = StdRandom.uniform(N);
         Item value = q[rand];
-        q[rand] = null;
+        //q[rand] = null;
+        if(rand != N-1) {q[rand] = q[N-1];}
+        q[N-1] = null;
         N--;
-        if(N > 0 && N == q.length/4)
+        if(N > 0 && N <= q.length/4)
         {
             resize(q.length/2);
         }
@@ -55,6 +69,10 @@ public class RandomizedQueue<Item> implements Iterable<Item>
     
     public Item sample()
     {
+        if(size() == 0)
+        {
+            throw new NoSuchElementException();
+        }
         int rand = StdRandom.uniform(N);
         Item value = q[rand];
         if(N > 0 && N == q.length/4)
@@ -64,6 +82,11 @@ public class RandomizedQueue<Item> implements Iterable<Item>
         return value;
     }
     
+//    private void findStartLoc()
+//    {
+//        
+//    }
+    
     public Iterator<Item> iterator()
     {
         return new RandomIterator();
@@ -71,35 +94,58 @@ public class RandomizedQueue<Item> implements Iterable<Item>
     
     private class RandomIterator implements Iterator<Item>
     {
-        private int currLoc = 0;
-        private int covered = 0;
+        private int randLoc = 0;
+        //private int covered = 0;
+        private Item[] copy = q;
+        private int copySize = copy.length;
         
         public boolean hasNext()
         {
-            return covered != q.length;
+            return copySize > 0;
         }
         
         public void remove()
         {
-            /* not supported */
+            throw new UnsupportedOperationException();
         }
         
         public Item next()
         {
-            currLoc = StdRandom.uniform(N);
-            Item currentItem = q[currLoc];
-            covered++;
+            if(copySize == 0)
+            {
+                throw new NoSuchElementException();
+            }
+            randLoc = StdRandom.uniform(N);
+            Item currentItem = q[randLoc];
+            if(randLoc != copySize-1)
+            {
+                q[randLoc] = q[copySize-1];
+            }
+            q[copySize-1] = null;
+            copySize--;
             return currentItem;
         }
     }
     
     public static void main(String[] args)
     {
-        RandomizedQueue<String> test = new RandomizedQueue<String>();
-        test.enqueue("to");
-        test.enqueue("be");
-        test.enqueue("or");
-        test.enqueue("not");
-        System.out.print(test.sample());
+//        RandomizedQueue<String> test = new RandomizedQueue<String>();
+//        test.enqueue("to");
+//        test.enqueue("be");
+//        test.enqueue("or");
+//        test.enqueue("not");
+//        System.out.print(test.sample());
+        
+         RandomizedQueue<String> test2 = new RandomizedQueue<String>();
+         test2.enqueue("to");
+         test2.enqueue("be");
+         test2.enqueue("or");
+         test2.enqueue("not");
+         test2.enqueue("to2");
+         test2.enqueue("lol2");
+         test2.enqueue("roll2");
+         test2.dequeue();
+         test2.dequeue();
+         
     }
 }
